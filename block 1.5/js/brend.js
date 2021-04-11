b7b5d8139f580cf4488a348e5170bb829835fe68
oksana <Okcasha42@gmail.com>
2021-04-11 16:06:52 +0300

    fix swiper 11.04

--- block 1.5/js/brend.js
+++ block 1.5/js/brend.js
@@ -1,9 +1,41 @@
-new Swiper(".brend-slider", {
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+var swiper = null;
+function slider() {
+    if (window.innerWidth <= 767) {
+         swiper = new Swiper(".brend-slider", {
+            slidesPerView: 'auto',
+            slidesOffsetBefore: 10,
+            slidesOffsetAfter: -200,
+            spaceBetween: 17,
+            pagination: {
+                el: ".swiper-pagination",
+                type: 'bullets',
+                clickable: true,
+                tabIndex: true
+            },
+            // breakpoints: {
+            //     // when window width is >= 480px
+            //     480: {
+            //         slidesPerView: 'auto',
+            //         spaceBetween: 30,
+            //     },
+            //     // when window width is >= 640px
+            //     640: {
+            //         slidesPerView: 'auto',
+            //         spaceBetween: 40,
+            //     }
+            // }
+        });
     }
-});
+}
+
+slider();
+
+var bullets = document.querySelector('.swiper-pagination-bullets').children;
+document.addEventListener('DOMContentLoaded', function(){
+    for (let i = 0; i < bullets.length; i++) {
+        bullets[i].setAttribute('id', i);
+    }
+})
 
 var button = document.querySelector('.btn-icon');
 var text = document.querySelector('.brend-text');
@@ -32,4 +64,16 @@ button.addEventListener('click', function () {
         attribute = true;
     }
 })
+
+swiper.on('slideChange', function () {
+    var activeBullet = document.querySelector('.swiper-pagination-bullet-active');
+    var id = activeBullet.getAttribute('id');
+    var translate = 0;
+    if (window.innerWidth > 0) {
+         translate = id * -270;
+        swiper.setTranslate(translate, 0);
+    }
+
+ });
+
 
